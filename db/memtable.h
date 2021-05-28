dbe55d1040336e6f19fad80299b48d0d0bb199d3
Xinjing Zhou <zxjcarrot@gmail.com>
2021-05-28 20:23:49 +0800

    Add CompressedMemTable.

--- db/memtable.h
+++ db/memtable.h
@@ -30,6 +30,9 @@ public:
     virtual void Unref() = 0;
     virtual size_t ApproximateMemoryUsage() = 0;
     virtual bool Get(const LookupKey& key, std::string* value, Status* s) = 0;
+    virtual bool GetField(const LookupKey& key, int ith, std::string *value, Status *s) {
+        throw std::runtime_error("Not implemented");
+    }
 };
 
 class MemTable : public AbstractMemTable {
@@ -86,6 +89,7 @@ class MemTable : public AbstractMemTable {
   friend class MemTableIterator;
   friend class MemTableBackwardIterator;
   friend class CompactConstMemTable;
+  friend class CompressedMemTable;
   typedef SkipList<const char*, KeyComparator> Table;
 
   KeyComparator comparator_;
@@ -153,6 +157,61 @@ private:
     // No copying allowed
     CompactConstMemTable(const CompactConstMemTable&);
     void operator=(const CompactConstMemTable&);
+};
+
+class CompressedMemTable : public AbstractMemTable {
+public:
+    // MemTables are reference counted.  The initial reference count
+    // is zero and the caller must call Ref() at least once.
+    explicit CompressedMemTable(MemTable& memtable);
+
+    // Increase reference count.
+    void Ref() { ++refs_; }
+
+    // Drop reference count.  Delete if no more references exist.
+    void Unref() {
+        --refs_;
+        assert(refs_ >= 0);
+        if (refs_ <= 0) {
+            delete this;
+        }
+    }
+
+    // Returns an estimate of the number of bytes of data in use by this
+    // data structure. It is safe to call when MemTable is being modified.
+    size_t ApproximateMemoryUsage();
+
+    // Return an iterator that yields the contents of the memtable.
+    //
+    // The caller must ensure that the underlying MemTable remains live
+    // while the returned iterator is live.  The keys returned by this
+    // iterator are internal keys encoded by AppendInternalKey in the
+    // db/format.{h,cc} module.
+    Iterator* NewIterator();
+
+    // If memtable contains a value for key, store it in *value and return true.
+    // If memtable contains a deletion for key, store a NotFound() error
+    // in *status and return true.
+    // Else, return false.
+    bool Get(const LookupKey& key, std::string* value, Status* s);
+
+    bool Get(const LookupKey& key, int ith, std::string* value, Status* s);
+private:
+    ~CompressedMemTable();  // Private since only Unref() should be used to delete it
+
+    typedef MemTable::KeyComparator KeyComparator;
+
+    friend class CompactConstMemTableIterator;
+    friend class CompactConstMemTableBackwardIterator;
+
+    KeyComparator comparator_;
+    int refs_;
+    Arena arena_;
+
+    // No copying allowed
+    CompressedMemTable(const CompressedMemTable&);
+    void operator=(const CompressedMemTable&);
+
 };
 }  // namespace leveldb
 
